78e3e5f8a15d4213f3920b0dcf2b6b6644d6e495
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-10-06 15:57:01 -0700

    styled form

--- handlers.js
+++ handlers.js
@@ -12,7 +12,7 @@ import api from './api.js';
 //------- | Setup Local Store From Server | ----------------------------------
 // I'm unsure where this would land in the Model, View Controller method.
 // This function takes the content of the API at the load of the page and populates our LOCALSTORE  with it's contents.
-let createLocalStore = function(){
+const createLocalStore = function(){
   api.getItems()
     .then(bookmark => Object.assign(Store.LOCALSTORE.bookmarks,bookmark))
     .then(() => Store.LOCALSTORE.bookmarks.forEach(bookmark => {
@@ -29,7 +29,7 @@ let createLocalStore = function(){
 
 //------- | Listener Statements | ----------------------------------
 //Need to review the best practice for the render statement.
-let handleNewBookmarkButton = function(){
+const handleNewBookmarkButton = function(){
   $('.primary-container').on('click','.new-bookmark-button',function(event){
     event.preventDefault();
     //currently this is removing the code because the bookmark list is in main-headers
@@ -41,7 +41,7 @@ let handleNewBookmarkButton = function(){
 };
 
 // change the store and render. That's it. 
-let handleCancelButton = function(){
+const handleCancelButton = function(){
   $('.cancel-button').on('click', function(event){
     event.preventDefault();
     Store.adding = false;
@@ -53,7 +53,7 @@ let handleCancelButton = function(){
 
 
 // Currently not working. 
-let handleExpand = function(){
+const handleExpand = function(){
   $('.primary-container').on('click','.js-bookmark .expand',  event => {
     event.preventDefault();
     let id = event.currentTarget.id;
@@ -87,7 +87,7 @@ let handleExpand = function(){
 
 
 
-let handleDelete = function () {
+const handleDelete = function () {
   $('.primary-container').on('click','span', event => {
     event.preventDefault();
     let id = event.currentTarget.id;
@@ -104,7 +104,7 @@ let handleDelete = function () {
   });
 };
 
-let handleSubmitButton = function(){
+const handleSubmitButton = function(){
   $('main').submit('#form',event => {
     event.preventDefault();
     let formElement = $('#form')[0];
@@ -125,7 +125,7 @@ let handleSubmitButton = function(){
 
 
 //Possible code for filter.!!!!!!----------------
-let handleFilterChange = function(){
+const handleFilterChange = function(){
   $('.primary-container').on('change','.filter-options', ( event => {
     event.preventDefault(); 
     Store.LOCALSTORE.filter = $('.filter-options').val();
@@ -136,7 +136,7 @@ let handleFilterChange = function(){
 
 
 
-let serializeJson = function(form){
+const serializeJson = function(form){
   let formData = new FormData(form);
   let o = {};
   formData.forEach((val,name) => o[name]= val);
@@ -173,7 +173,7 @@ const generateExpandedView = function(bookmark){
       `);
 };
 
-let generateBookmarkCompressedElement = function (bookmark) {
+const generateBookmarkCompressedElement = function (bookmark) {
   //console.log(bookmark);
   $('.placeholder').append(`
     <div class="bookmark-element js-bookmark collapsed" id="${bookmark.id}">
@@ -184,7 +184,7 @@ let generateBookmarkCompressedElement = function (bookmark) {
   
 };
 
-let renderBookmarkList = function(){
+const renderBookmarkList = function(){
   $('.placeholder').html(''); 
   let localBookmarks = Store.LOCALSTORE.bookmarks;
   for (let i = 0; i < localBookmarks.length;  i++){
@@ -207,10 +207,10 @@ let renderBookmarkList = function(){
 
 };
 
-let renderFormOrHeaders = function() {
+const renderFormOrHeaders = function() {
   if (Store.adding){
     $('.primary-container').html(`
-    <form id="form">
+    <form class="form" id="form">
     <fieldset class="fieldset">
       <div>
         <label>Title</label>
@@ -259,7 +259,7 @@ let renderFormOrHeaders = function() {
 
 //------- | binding the listeners | ----------------------------------
 
-let callListeners = function(){
+const callListeners = function(){
   handleNewBookmarkButton();
   handleDelete();
   handleFilterChange();
